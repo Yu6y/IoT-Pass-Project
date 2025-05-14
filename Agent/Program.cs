81319eea0e66d647bed02f3bcb215475e93c2a7e
KacperGrodzicki <ul0266227@edu.uni.lodz.pl>
2025-05-14 09:20:40 +0200

    Multiple devices (still need to implement in Direct Methods and Twin)

--- Agent/Program.cs
+++ Agent/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Devices.Client;
+﻿// Program.cs
+using Microsoft.Azure.Devices.Client;
 using Device;
 using Opc.UaFx.Client;
 using Opc.UaFx;
@@ -13,8 +14,7 @@ namespace Agent
     {
         static async Task Main(string[] args)
         {
-            string deviceConnectionString = "HostName=IoTZajecia.azure-devices.net;DeviceId=test_device;SharedAccessKey=g/tfP0og9Qm3vGzht2vGFEA1T7volalBQBXPickK9SU=";
-
+            string deviceConnectionString = "";
             using var opcClient = new OpcClient("opc.tcp://localhost:4840/");
             using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString);
 
@@ -30,40 +30,49 @@ namespace Agent
 
                 while (true)
                 {
-                    try
+                    OpcNodeInfo objectsNode = opcClient.BrowseNode(OpcObjectTypes.ObjectsFolder);
+
+                    foreach (var childNode in objectsNode.Children())
                     {
-                        OpcReadNode[] nodes = new OpcReadNode[]
+                        if (childNode.DisplayName.Value.StartsWith("Device"))
                         {
-                            new OpcReadNode("ns=2;s=Device 1/ProductionStatus"),
-                            new OpcReadNode("ns=2;s=Device 1/ProductionRate"),
-                            new OpcReadNode("ns=2;s=Device 1/WorkorderId"),
-                            new OpcReadNode("ns=2;s=Device 1/Temperature"),
-                            new OpcReadNode("ns=2;s=Device 1/GoodCount"),
-                            new OpcReadNode("ns=2;s=Device 1/BadCount"),
-                            new OpcReadNode("ns=2;s=Device 1/DeviceError"),
-                        };
+                            try
+                            {
+                                Console.WriteLine(childNode.DisplayName.Value);
+                                OpcReadNode[] nodes = new OpcReadNode[]
+                                {
+                                new OpcReadNode($"ns=2;s={childNode.DisplayName.Value}/ProductionStatus"),
+                                new OpcReadNode($"ns=2;s={childNode.DisplayName.Value}/ProductionRate"),
+                                new OpcReadNode($"ns=2;s={childNode.DisplayName.Value}/WorkorderId"),
+                                new OpcReadNode($"ns=2;s={childNode.DisplayName.Value}/Temperature"),
+                                new OpcReadNode($"ns=2;s={childNode.DisplayName.Value}/GoodCount"),
+                                new OpcReadNode($"ns=2;s={childNode.DisplayName.Value}/BadCount"),
+                                new OpcReadNode($"ns=2;s={childNode.DisplayName.Value}/DeviceError"),
+                                };
 
-                        var values = opcClient.ReadNodes(nodes).ToArray();
+                                var values = opcClient.ReadNodes(nodes).ToArray();
 
-                        var data = new DeviceData
-                        {
-                            ProductionStatus = (int)values[0].Value,
-                            ProductionRate = (int)values[1].Value,
-                            WorkerId = (string)values[2].Value,
-                            Temperature = (double)values[3].Value,
-                            GoodCount = (long)values[4].Value,
-                            BadCount = (long)values[5].Value,
-                            DeviceErrors = (int)values[6].Value
-                        };
+                                var data = new DeviceData
+                                {
+                                    ProductionStatus = (int)values[0].Value,
+                                    ProductionRate = (int)values[1].Value,
+                                    WorkerId = (string)values[2].Value,
+                                    Temperature = (double)values[3].Value,
+                                    GoodCount = (long)values[4].Value,
+                                    BadCount = (long)values[5].Value,
+                                    DeviceErrors = (int)values[6].Value
+                                };
 
-                        await device.SendMessages(data);
-                        await device.UpdateTwinAsync(data);
+                                await device.SendMessages(data);
+                                await device.UpdateTwinAsync(data);
 
-                        Console.WriteLine($"{DateTime.Now:HH:mm:ss} - Data sent.\n");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"[ERROR] OPC UA read/send failed: {ex.Message}");
+                                Console.WriteLine($"{DateTime.Now:HH:mm:ss} - Device {childNode.NodeId.ToString()} data sent.\n");
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"[ERROR] Device {childNode.NodeId.ToString()} OPC UA read/send failed: {ex.Message}");
+                            }
+                        }
                     }
 
                     await Task.Delay(TimeSpan.FromSeconds(5));
